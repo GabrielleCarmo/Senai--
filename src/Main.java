4d18859d3ce09dac131248ce60e9e6439ec97e24
Gabrielle Carmo <gabrielles200@gmail.com>
2025-05-24 17:10:51 -0300

    feat:Aula inicial java senai

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        String nome = "Gabrielle";
+
+        int valor = 10;
+        int valor01 = 17;
+        int valor02 = 7;
+
+        double valor04 = 10;
+        double valor05 = 7;
+
+        System.out.println(valor + valor01);
+        System.out.println(valor01 - valor02);
+        System.out.println(valor04 / valor05);
+        System.out.println(valor * valor01);
+        System.out.println(valor % valor02);
+
+
+
+
+        // Principais tipos primitivos //
+
+        String letra = "ttttttttttt";
+        int sup = 999999999;
+        Long sup01 = 999999999999999999L;
+
+        float sup02 = 99999999999999999999999999999999999999.9F;
+        Double sup03 = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999.9;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println(nome);
+
+
+
+
+
+
+
+
+
+    }
+}
